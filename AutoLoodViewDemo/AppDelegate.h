32942ab016ca7dcd0f6640a1f1bc21e0b35d8a82
wzhen22 <qwaszx375204@126.com>
2014-03-10 18:09:31 -0700

    Initial Commit

---
+++ AutoLoodViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoLoodViewDemo
+//
+//  Created by 传晟 on 14-3-10.
+//  Copyright (c) 2014年 传晟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutoLoodViewDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutoLoodViewDemo
+//
+//  Created by 传晟 on 14-3-10.
+//  Copyright (c) 2014年 传晟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
